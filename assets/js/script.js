a380a06d8baaed68fb0010e61505fdcb084bb4a5
KW <kenneth_sl_wong@msn.com>
2022-02-10 13:57:53 -0500

    added object for player character and switched to display combat buttons and screen after game start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
+//variable definitions
+var playerChar = {
+    health: 100,
+    attack: 10,
+    credits: 10
+};
+
 //New game function
 var newGame = function(event){
     event.preventDefault();
@@ -7,9 +14,23 @@ var newGame = function(event){
 
     if(!playerName || playerName.length > 15){
         $("#initial-text").text("Please enter a valid name under 15 characters.")
+        return false;
     }
+
+    playerChar.name = playerName;
+
+    //hide initial button and display combat buttons
+    $("#initial-message").removeClass("d-flex");
+    $("#initial-message").addClass("d-none");
+    $("#combat-screen").removeClass("d-none");
+    $("#combat-screen").addClass("d-flex");
+    $("#initial-buttons").removeClass("d-flex");
+    $("#initial-buttons").addClass("d-none");
+    $("#combat-buttons").removeClass("d-none");
+    $("#combat-buttons").addClass("d-flex");
 }
 
+//event listeners
 //event listeners for new game
 $("#name-confirm").click(newGame);
 $("#name-input-form").submit(newGame);
